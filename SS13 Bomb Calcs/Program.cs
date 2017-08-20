94225b88980cb362f21fcc60feb58437e69e5759
Laurie Kirkcaldy - Shadow <lauriekirkcaldy@gmail.com>
2017-08-20 13:33:36 +0100

    Add project files.

---
+++ SS13 Bomb Calcs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SS13_Bomb_Calcs {
+	class Program {
+		static void Main(string[] args) {
+		}
+	}
+}
